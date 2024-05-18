27b862208186891f7e74bbeddd0ad5e3c1ac233d
k <ktf.ahmynket5317@gmail.com>
2024-05-18 07:48:38 +0000

    once finish

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'books/edit'
   post 'books' => 'books#create'
   get '/top' => 'homes#top'
+  root 'home#index'
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
