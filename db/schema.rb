d739e3fc80ea128b71479e9d7ecba8ca2ee47bf3
k <ktf.ahmynket5317@gmail.com>
2024-05-20 10:38:17 +0000

    once finish5/20-1

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_16_185211) do
+ActiveRecord::Schema.define(version: 2024_05_19_043108) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
