8a937369646fded37d213065f531440eaf900663
k <ktf.ahmynket5317@gmail.com>
2024-06-01 08:46:04 +0000

    once finish 6/1 17:45

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get '/top' => 'homes#top'
   get 'books/:id' => 'books#show',as:'book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
