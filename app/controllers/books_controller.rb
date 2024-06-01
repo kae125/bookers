8dd174d1e9e0ca2689d6deb32ae7cae7ca251df8
k <ktf.ahmynket5317@gmail.com>
2024-06-01 07:41:26 +0000

    once finish 6/1 16:40

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,17 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
   
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   def create
     @book = Book.new(book_params)
-    if @book.save
+    @book.save
     redirect_to  book_path(@book.id)
-    else
-    render 'new'
-    end
   end
   
   
