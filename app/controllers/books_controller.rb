75d247d9cdfba55baa563822abea96c872fc35dd
k <ktf.ahmynket5317@gmail.com>
2024-06-02 13:01:19 +0000

    once finish 6/2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+
   def new
     @book = Book.new
   end
@@ -17,16 +18,33 @@ class BooksController < ApplicationController
   
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = @book.user
+    @comment = BookComment.new
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user #URLを入力しても画面に飛ばせない
+       render "edit"
+    else
+       redirect_to books_path
+    end
   end
   
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book),notice: 'You have updated book successfully.'
+    else
+       render "edit"
+    end
+  end
+  
+  def destroy
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    book.destroy
+    redirect_to books_path
   end
   
   private
