4b8e165c7db8660a68d62036ecf5b74266fb950d
k <ktf.ahmynket5317@gmail.com>
2024-06-09 20:59:59 +0000

    6/10 6:00

--- db/migrate/20240519043108_create_books.rb
+++ db/migrate/20240519043108_create_books.rb
@@ -1,9 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
-    create_table :books do |t|
+    create_table :designs do |t|
       t.string :title
       t.text :body
-
       t.timestamps
     end
   end
