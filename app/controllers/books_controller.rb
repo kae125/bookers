3ffe078c11f80fee4f161e2fb35ee0829803f4c4
k <ktf.ahmynket5317@gmail.com>
2024-06-10 12:11:28 +0000

    6/10 21:00

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,9 +26,9 @@ class BooksController < ApplicationController
   end
   
   def update
-   book = Book.find(params[:id])
-   book = update(book_params)
-   redirect_to book_path(book.id),notice: 'You have updated book successfully.'
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id),notice: 'You have updated book successfully.'
   end
   
   def destroy
