58c2f359eccc3789d7340e641d7976e9eed0500d
k <ktf.ahmynket5317@gmail.com>
2024-06-14 09:29:04 +0000

    6/14 18:30

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,8 +11,12 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to  book_path(@book.id)
+    if @book.save
+       flash[:notice] = "Book was successfully created."
+       redirect_to  book_path(@book.id)
+    else
+       render :index
+    end
   end
   
   
@@ -28,13 +32,13 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id),notice: 'You have updated book successfully.'
+    redirect_to book_path(book.id),notice: 'Book was successfully updated.'
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to book_path
   end
   
   private
