a73759d66261f4a3f137147077b5a58cb4270e7d
k <ktf.ahmynket5317@gmail.com>
2024-06-16 11:44:43 +0000

    6/16 20:40

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   has_one_attached :image
 
- validates :title, presence: { message: 'error: Title cannot be blank.' }
+ validates :title, presence: { message: 'error: Title cannott be blank.' }
  validates :body, presence: { message: 'error: Body cannot be blank.' }
 end
