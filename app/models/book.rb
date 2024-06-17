6636fced9579333015a9a41af96751f7e641e366
k <ktf.ahmynket5317@gmail.com>
2024-06-17 19:45:56 +0000

    6/18 5:00 Rspec once finish

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   has_one_attached :image
 
- validates :title, presence: { message: 'error: Title cannott be blank.' }
- validates :body, presence: { message: 'error: Body cannot be blank.' }
+ validates :title, presence: { message: "error: Title can't be blank" }
+ validates :body, presence: { message: "error: Body can't be blank" }
 end
