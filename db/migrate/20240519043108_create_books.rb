01591258790331eff69ba78d237bfe03afd9cae7
k <ktf.ahmynket5317@gmail.com>
2024-06-17 12:02:29 +0000

    6/16 21:00 Rspec once finish

--- db/migrate/20240519043108_create_books.rb
+++ db/migrate/20240519043108_create_books.rb
@@ -1,6 +1,6 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
-    create_table :designs do |t|
+    create_table :books do |t|
       t.string :title
       t.text :body
       t.timestamps
